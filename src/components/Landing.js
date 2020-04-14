0e1d5d512a616761e02e64e2e57c9a3d40e6ca02
Sergio Rivas <sergio.rivas@pucp.pe>
2020-04-14 00:37:21 -0500

    :art: Primeros retoques de la marca

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -8,7 +8,11 @@ import ProductList from './product/ProductList';
 
 const Hero = styled.div `
   height: 300px;
-  background: #aaa;
+  background-image: url("https://scontent.ftru1-1.fna.fbcdn.net/v/t1.0-9/71242309_729936210784589_4771595630906703872_n.png?_nc_cat=107&_nc_sid=dd9801&_nc_eui2=AeGoUxyMbwOhlXRcsHNmsAnK6ko9z2G-MWbqSj3PYb4xZrOhwP37IamOr-VEIHSQ5mw&_nc_ohc=VMImlDZ9sVkAX-ebYlg&_nc_ht=scontent.ftru1-1.fna&oh=244c7ceaa4239e556fcf691977850857&oe=5EB95907");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: #cccccc;
+  background-size: cover;
   color: white;
   font-size: 20px;
   display: flex;
@@ -22,7 +26,7 @@ const Landing = ({ config }) => (
     <Paper style={{ padding: "40px" }}>
       <Hero>
         <div style={{ display: "inline-block", maxWidth: "80%"}}>
-          <p>intro your site here!</p>
+          <h1>Los mejores vapes y líquidos del mercado</h1>
         </div>
       </Hero>
       <Divider style={{ margin: "40px 0" }}/>
