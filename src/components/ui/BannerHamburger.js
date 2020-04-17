3be05acb533b649094b05c82f97c819c9d34aa81
Sergio R <sergio.rivas@pucp.pe>
2020-04-17 14:10:22 -0500

    :rocket: cambio a port 5000 para parte admin development

---
+++ src/components/ui/BannerHamburger.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const styles = theme => ({
+  hamburger: {
+    flex: 1,
+    textAlign: "right"
+  },
+  hamburgerButton: {
+    textDecoration: "none",
+    width: "100%"
+  },
+});
+
+function BannerHamburger({ classes, productLink, aboutLink, number }) {
+  const [menu, toggleMenu] = useState(false);
+
+  return (
+    <span className={classes.hamburger}>
+      <IconButton
+        color="inherit" aria-label="Menu"
+        onClick={() => toggleMenu(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SwipeableDrawer anchor="right"
+        open={menu}
+        onClose={() => toggleMenu(false)}
+        onOpen={() => toggleMenu(true)}
+      >
+        <List style={{ width: "200px" }}>
+          <ListItem>
+            <Link
+              to={`/`}
+              className={classes.hamburgerButton}
+              onClick={() => toggleMenu(false)}
+            >
+              <Typography variant="button" gutterBottom>Home</Typography>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <span
+              onClick={() => toggleMenu(false)} 
+              style={{ display: "flex", width: "100%" }}
+            >
+              {productLink}
+            </span>
+          </ListItem>
+          <ListItem>
+            <span
+              onClick={() => toggleMenu(false)} 
+              style={{ display: "flex", width: "100%" }}
+            >
+              {aboutLink}
+            </span>
+          </ListItem>
+          <ListItem>
+            <Link
+              to={`/cart`}
+              className={classes.hamburgerButton}
+              onClick={() => toggleMenu(false)}
+            >
+              <Typography variant="button" gutterBottom>
+                Cart{number}
+              </Typography>
+            </Link>
+          </ListItem>
+        </List>
+      </SwipeableDrawer>
+    </span>
+  );
+};
+export default withStyles(styles)(BannerHamburger);
