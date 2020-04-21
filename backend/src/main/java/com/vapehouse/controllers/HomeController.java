b56ca59e371c403557636f01f160caa77ebcd6bc
Sergio R <sergio.rivas@pucp.pe>
2020-04-21 16:43:34 -0500

    :tada: backend

---
+++ backend/src/main/java/com/vapehouse/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.vapehouse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value="/")
+    public String index() {
+        return "index";
+    }
+    
+}
