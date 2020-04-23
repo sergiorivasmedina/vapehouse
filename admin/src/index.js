662e93647021feb7d8d5da77c7be93a22e5eaa5d
Sergio R <sergio.rivas@pucp.pe>
2020-04-23 18:38:36 -0500

    corrigiendo todo

---
+++ admin/src/index.js
@@ -0,0 +1,22 @@
+import 'react-app-polyfill/ie9'; // For IE 9-11 support
+import 'react-app-polyfill/stable';
+// import 'react-app-polyfill/ie11'; // For IE 11 support
+import './polyfill'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+export default axios.create({
+    baseURL: 'http://35.202.103.227:9000'
+    //baseURL: 'http://localhost:9000'
+})
