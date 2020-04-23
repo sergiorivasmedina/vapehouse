d81b07969a22569fa9ab8f0ed04dfaa59f9ffc47
Sergio R <sergio.rivas@pucp.pe>
2020-04-23 17:56:06 -0500

    :sparkles: editar productos - admin

--- admin/src/Components/LineaProducto/LineaProducto.js
+++ admin/src/Components/LineaProducto/LineaProducto.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Label,FormGroup, Input,Button,Col,Modal,ModalBody,ModalHeader,ModalFooter } from 'reactstrap';
-import axios from '../../index';
+import { Button,Modal,ModalBody,ModalHeader,ModalFooter } from 'reactstrap';
 
 class LineaProducto extends Component {
     constructor(props){
@@ -8,93 +7,29 @@ class LineaProducto extends Component {
         this.state = {
             primary: false,
             primary2: false,
-            productoModal: false,
-            idProducto: null,
-            valueEditarProductoNombre: this.props.nombre,
-            valueEditarProductoNOrden: this.props.norden,
-            valueEditarProductoPrecio: this.props.precio,
-            valueEditarProductoDescripcion: this.props.descripcion,
-
-            productChange: false
-        };
+        }
+        
 
         this.togglePrimary = this.togglePrimary.bind(this);
         this.togglePrimary2 = this.togglePrimary2.bind(this);
-        this.toggleProducto = this.toggleProducto.bind(this);
-
-        this.handleChangeEditarProductoNombre = this.handleChangeEditarProductoNombre.bind(this);
-        this.handleChangeEditarProductoNOrden = this.handleChangeEditarProductoNOrden.bind(this);
-        this.handleChangeEditarProductoPrecio = this.handleChangeEditarProductoPrecio.bind(this);
-        this.handleChangeEditarProductoDescripcion = this.handleChangeEditarProductoDescripcion.bind(this);
-    }
-
-    handleChangeEditarProductoNombre(event) {
-        this.setState({valueEditarProductoNombre:event.target.value});
-    }
-
-    handleChangeEditarProductoNOrden(event) {
-        this.setState({valueEditarProductoNOrden:event.target.value});
-    }
-
-    handleChangeEditarProductoPrecio(event) {
-        this.setState({valueEditarProductoPrecio:event.target.value});
-    }
-
-    handleChangeEditarProductoDescripcion(event) {
-        this.setState({valueEditarProductoDescripcion:event.target.value});
     }
 
     togglePrimary() {
         this.setState({
-          primary: !this.state.primary,
+            primary: !this.state.primary,
         });
-      }
+    }
     
-      togglePrimary2() {
+    togglePrimary2() {
         this.setState({
-          primary2: !this.state.primary2,
+        primary2: !this.state.primary2,
         });
-      }
-    
-      toggleProducto() {
-        this.setState({
-          productoModal: !this.state.productoModal,
-        });
-      }
-
-      componentDidMount(){
-      }
-
-      abrirProductoConfig() {
-        console.log(this.props.idProducto);
-
-        this.toggleProducto();
-      }
-
-      guardar() {
-        if(document.getElementById('nuevoNombre').value != this.props.nombre || document.getElementById('nuevoNOrden').value != this.props.norden || document.getElementById('nuevoPrecio').value != this.props.precio || this.props.nombre && document.getElementById('nuevaDescripcion').value != this.props.descripcion){
-            //llamar al web service correspondiente para la actualizacion del producto
-            var producto = {
-                idProducto: this.props.idProducto,
-                nombre: document.getElementById('nuevoNombre').value,
-                precio: document.getElementById('nuevoPrecio').value,
-                norden: document.getElementById('nuevoNOrden').value,
-                descripcion: document.getElementById('nuevaDescripcion').value
-            }
-
-            axios.post('/producto/editar', producto).then(res =>{
-                console.log("Se realizo el cambio del producto");
-            })
-        }
-        
-        
-        this.toggleProducto();
-      }
+    }
 
     render() {
         
         return(
-            <tr>
+            <>
                 <td>{this.props.norden}</td>
                 <td>{this.props.nombre}</td>
                 <td>{this.props.precio}</td>
@@ -140,44 +75,7 @@ class LineaProducto extends Component {
                     </ModalFooter>
                     </Modal>
                 </td>
-                <td>
-                    <Button color="danger" className="btn-pill" onClick={this.abrirProductoConfig.bind(this)}>
-                    <i className="fa fa-edit"></i>&nbsp;Editar Producto
-                    </Button>
-                    <Modal isOpen={this.state.productoModal} toggle={this.toggleProducto}
-                        className={'modal-primary ' + this.props.className}>
-                    <ModalHeader toggle={this.toggleProducto}>Editar información del producto</ModalHeader>
-                    <ModalBody>
-                        <FormGroup row>
-                        <Col sm="3">
-                            <Label htmlFor="norden">N° Orden</Label>
-                            <Input type="text" id="nuevoNOrden" placeholder="N° Orden" value={this.state.valueEditarProductoNOrden} onChange={this.handleChangeEditarProductoNOrden}/>
-                        </Col>
-                        <Col sm="4">
-                            <Label htmlFor="precio">Precio</Label>
-                            <Input type="text" id="nuevoPrecio" placeholder="Nuevo precio" value={this.state.valueEditarProductoPrecio} onChange={this.handleChangeEditarProductoPrecio}/>
-                        </Col>
-                        </FormGroup>
-                        <FormGroup>
-                        <Col sm="10">
-                            <Label htmlFor="nombre">Nombre</Label>
-                            <Input type="text" id="nuevoNombre" placeholder="Ingrese nuevo nombre" value={this.state.valueEditarProductoNombre} onChange={this.handleChangeEditarProductoNombre}/>
-                        </Col>
-                        </FormGroup>
-                        <FormGroup>
-                        <Col sm="12">
-                            <Label htmlFor="descripcion">Descripción</Label>
-                            <Input type="text" id="nuevaDescripcion" placeholder="Ingrese nueva descripción" value={this.state.valueEditarProductoDescripcion} onChange={this.handleChangeEditarProductoDescripcion}/>
-                        </Col>
-                        </FormGroup>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={this.guardar.bind(this)}>Guardar</Button>{' '}
-                        <Button color="secondary" onClick={this.toggleProducto}>Cancel</Button>
-                    </ModalFooter>
-                    </Modal>
-                </td>
-            </tr>
+            </>
         );
     }
 }
--- admin/src/views/Gestion/Productos/Productos.js
+++ admin/src/views/Gestion/Productos/Productos.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import { Card, CardBody, CardHeader, Col, Row, Table, Input,Button } from 'reactstrap';
 import LineaProducto from '../../../Components/LineaProducto/LineaProducto'
 import axios from '../../../index';
 
@@ -11,19 +11,99 @@ class Producto extends Component {
     this.state = {
       productos: [],
       
+      productoModal: false,
+      valueEditarProductoNombre: '',
+      valueEditarProductoNOrden: '',
+      valueEditarProductoPrecio: '',
+      valueEditarProductoDescripcion: '',
+      idProducto: -1,
+
+      productChange: false
+      
     };
 
+    
+    this.toggleProducto = this.toggleProducto.bind(this);
+
+    this.handleChangeEditarProductoNombre = this.handleChangeEditarProductoNombre.bind(this);
+    this.handleChangeEditarProductoNOrden = this.handleChangeEditarProductoNOrden.bind(this);
+    this.handleChangeEditarProductoPrecio = this.handleChangeEditarProductoPrecio.bind(this);
+    this.handleChangeEditarProductoDescripcion = this.handleChangeEditarProductoDescripcion.bind(this);
     
   }
 
   componentDidMount(){
     //aqui traer la informacion del back-end
-    console.log("probando componentDidMount");
     axios.get('/productos').then(res => {
       this.setState({ productos:res.data })
     })
 
+  }
+
+  handleChangeEditarProductoNombre(event) {
+    this.setState({valueEditarProductoNombre:event.target.value});
+  }
+
+  handleChangeEditarProductoNOrden(event) {
+      this.setState({valueEditarProductoNOrden:event.target.value});
+  }
+
+  handleChangeEditarProductoPrecio(event) {
+      this.setState({valueEditarProductoPrecio:event.target.value});
+  }
+
+  handleChangeEditarProductoDescripcion(event) {
+      this.setState({valueEditarProductoDescripcion:event.target.value});
+  }
+
+  
+
+  toggleProducto() {
+    this.setState({
+      productoModal: !this.state.productoModal,
+    });
+  }
+
+  abrirProductoConfig(producto) {
+    this.setState({idProducto:producto.idProducto});
     
+    //asignar los valores a los inputs
+    this.setState({valueEditarProductoNOrden: producto.norden});
+    this.setState({valueEditarProductoNombre: producto.nombre});
+    this.setState({valueEditarProductoPrecio: producto.precio});
+    this.setState({valueEditarProductoDescripcion: producto.descripcion});
+  }
+
+  cancelarProductoConfig() {
+    this.setState({idProducto:-1});
+  }
+
+  guardar(producto) {
+    if(document.getElementById('nuevoNombre').value !== producto.nombre || document.getElementById('nuevoNOrden').value !== producto.norden || document.getElementById('nuevoPrecio').value !== producto.precio || document.getElementById('nuevaDescripcion').value !== producto.descripcion){
+      
+      //llamar al web service correspondiente para la actualizacion del producto
+        var prod = {
+            idProducto: producto.idProducto,
+            nombre: document.getElementById('nuevoNombre').value,
+            precio: document.getElementById('nuevoPrecio').value,
+            norden: document.getElementById('nuevoNOrden').value,
+            descripcion: document.getElementById('nuevaDescripcion').value,
+            fechaRegistro: producto.fechaRegistro
+        }
+
+        axios.post('/producto/editar', prod).then(res =>{
+            console.log("Se realizo el cambio del producto");
+            //actualizar la tabla
+            var index_actualizar = this.state.productos.findIndex(x=> x.idProducto === producto.idProducto);
+            let prodArray = this.state.productos.slice();
+            prodArray[index_actualizar].nombre = prod.nombre;
+            this.setState({productos: prodArray});
+            this.setState({idProducto: -1});
+        })
+    }
+    
+    
+    this.toggleProducto();
   }
 
     
@@ -52,31 +132,45 @@ class Producto extends Component {
                   </thead>
 
                   <tbody>
+                  
                     {this.state.productos.map(producto =>
-                      <LineaProducto idProducto={producto.idProducto} norden={producto.norden} nombre={producto.nombre} precio={producto.precio} descripcion={producto.descripcion} fechaRegistro={producto.fechaRegistro}/>
+                    <tr>
+                      {(this.state.idProducto !== producto.idProducto) ? (
+                        <React.Fragment>
+                          <LineaProducto idProducto={producto.idProducto} norden={producto.norden} nombre={producto.nombre} precio={producto.precio} descripcion={producto.descripcion} fechaRegistro={producto.fechaRegistro}/>
+                          <td>
+                              <Button color="danger" className="btn-pill" onClick={this.abrirProductoConfig.bind(this, producto)}>
+                              <i className="fa fa-edit"></i>&nbsp;Editar Producto
+                              </Button>
+                          </td>
+                        </React.Fragment>
+                      ):(
+                        <React.Fragment>
+                          <td><Input type="text" id="nuevoNOrden" placeholder="N° Orden" value={this.state.valueEditarProductoNOrden} onChange={this.handleChangeEditarProductoNOrden}/></td>
+                          <td><Input type="text" id="nuevoNombre" placeholder="Ingrese nombre" value={this.state.valueEditarProductoNombre} onChange={this.handleChangeEditarProductoNombre}/></td>
+                          <td><Input type="text" id="nuevoPrecio" placeholder="Ingrese precio" value={this.state.valueEditarProductoPrecio} onChange={this.handleChangeEditarProductoPrecio}/></td>
+                          <td><Input type="text" id="nuevaDescripcion" placeholder="Ingrese descripcion" value={this.state.valueEditarProductoDescripcion} onChange={this.handleChangeEditarProductoDescripcion}/></td>
+                          <td> &nbsp;&nbsp;&nbsp;&nbsp;</td>
+                          <td> &nbsp;&nbsp;&nbsp;&nbsp;</td>
+                          <td>
+                            <Button color="secondary" outline className="btn-pill" onClick={this.cancelarProductoConfig.bind(this, producto)}>
+                              <i className="fa fa-times-rectangle-o"></i>&nbsp;Cancelar
+                            </Button>
+                          </td>
+                          <td>
+                            <Button color="danger" outline className="btn-pill" onClick={this.guardar.bind(this, producto)}>
+                              <i className="fa fa-save"></i>&nbsp;Guardar
+                            </Button>
+                          </td>
+                        </React.Fragment>
+                      )}
+                      
+                      
+                    </tr>
                     )}
+                  
                   </tbody>
                 </Table>
-                <Pagination>
-                  <PaginationItem>
-                    <PaginationLink previous tag="button"></PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink tag="button">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">2</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">3</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink tag="button">4</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink next tag="button"></PaginationLink>
-                  </PaginationItem>
-                </Pagination>
               </CardBody>
             </Card>
           </Col>
