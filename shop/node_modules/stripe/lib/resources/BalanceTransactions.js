0f8736086c1bda4021a32b5344b422c19b631702
Sergio R <sergio.rivas@pucp.pe>
2020-04-26 23:50:48 -0500

    :fire: quitando node_module de shop

--- shop/node_modules/stripe/lib/resources/BalanceTransactions.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = require('../StripeResource').extend({
-  path: 'balance/history',
-  includeBasic: ['list', 'retrieve'],
-});
--- shop/node_modules/stripe/lib/resources/TaxRates.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = require('../StripeResource').extend({
-  path: 'tax_rates',
-  includeBasic: ['create', 'list', 'update', 'retrieve'],
-});
-
